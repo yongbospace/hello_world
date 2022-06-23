20ffb8c47c0a82e555f2d08f2def17f30d309778
yongboshim <yongbospace@gmail.com>
2022-06-23 14:52:22 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'Hello World!',
+            style: TextStyle(fontSize: 50),
+          ),
+        ),
+      ),
+    ),
+  );
+}
